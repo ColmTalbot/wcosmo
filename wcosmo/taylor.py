ce3e7dfaacb3bbc65a913267cd367b21d120955a
Amanda Farah <amanda.farah@ligo.org>
2024-06-29 16:12:46 -0500

    add examples to analytic integral docstring

--- wcosmo/taylor.py
+++ wcosmo/taylor.py
@@ -204,7 +204,63 @@ def analytic_integral(z, Om0, w0=-1, zpower=0):
     Returns
     -------
     integral: array_like
-        The integral of :math:`1 / E(z)` from :math:`\infty` to :math:`z`
+        The integral of :math:`(1 + z)^k / E(z)` from :math:`\infty` to :math:`z`
+
+    Examples
+    --------
+    We can use this to calculate many cosmological distances. For example, the 
+    comoving distance :math:`d_C` is given by
+
+    .. math::
+
+        d_{{C}} = d_{{H}} \int_{{0}}^{{z}}
+        \frac{{dz'}}{{E(z'; \Omega_{{m,0}}, w_0)}}
+
+    In this case, :math:`k=0`
+
+    >>> from wcosmo.taylor import analytic_integral
+    >>> import wcosmo
+
+    >>> analytic_integral(z=2,Om0=0.3,zpower=0) * wcosmo.hubble_distance(H0=70)
+
+    We can check this against the comoving volume given by ``astropy`` and see 
+    that it gives the same answer.
+
+    >>> from astropy.cosmology import FlatLambdaCDM
+    >>> ap_cosmo = FlatLambdaCDM(H0=70,Om0=0.3)
+    >>> ap_cosmo.comoving_distance(2)
+
+    This is how :function:`comoving_distance` is implemented in ``wcosmo``.
+
+    As another example, the lookback time :math:`t_L` is given by
+
+    .. math::
+
+        t_L = t_H\int_0^z \frac{dz'}{(1+z)E(z')}
+
+    where :math:`t_L` is the Hubble distance. In this case, :math:`k=-1`
+
+    >>> analytic_integral(z=2,Om0=0.3,zpower=-1) * wcosmo.hubble_time(70) # Gyr
+
+    We can again compare this to ``astropy``
+
+    >>> ap_cosmo.lookback_time(2)
+
+    Finally, we can calculate the absorption distance, :math:`X`, commonly used 
+    in absorption line spectroscopy. This is not a physical distance, but is still 
+    a very useful quantity.
+    See the book "Cosmological Absorption Line Spectroscopy" by Christopher W. 
+    Churchill for a full discussion.
+
+    .. math::
+
+        X(z, \Omega_m) = \int_0^{z} \frac{dz' (1+z')^2}{
+            \sqrt{\Omega_m(1+z'^3) + (1 − \Omega_m)}}
+
+    Here, :math:`k=2` and there are no factors of the Hubble distance.
+
+    >>> print(analytic_integral(z=2,Om0=0.3,zpower=2))
+    >>> print(ap_cosmo.absorption_distance(2))
     """
     kwargs = dict(Om0=Om0, w0=w0, zpower=zpower)
     return indefinite_integral(z, **kwargs) - indefinite_integral(0, **kwargs)

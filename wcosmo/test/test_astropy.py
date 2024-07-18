96fcd5796f3bc0e3b65f900070b056dbd094047e
Colm Talbot <talbotcolm@gmail.com>
2024-07-18 19:41:25 +0000

    TST: add reference to test

--- wcosmo/test/test_astropy.py
+++ wcosmo/test/test_astropy.py
@@ -1,4 +1,5 @@
 def test_astropy_cosmology_not_clobbered():
+    """See https://github.com/ColmTalbot/wcosmo/issues/15"""
     import wcosmo.astropy
     import astropy.cosmology
     assert "wcosmo" not in astropy.cosmology.Planck15.__module__

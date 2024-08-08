ec18166d7deba477127de4cb00edab0b084ba4ae
Amanda Farah <amanda.farah@ligo.org>
2024-08-08 12:06:22 -0500

    add extra braces to sub/superscripts and reformat with precommit version of black

--- wcosmo/taylor.py
+++ wcosmo/taylor.py
@@ -208,8 +208,8 @@ def analytic_integral(z, Om0, w0=-1, zpower=0):
 
     Examples
     --------
-    We can use this to calculate many cosmological distances. For example, the 
-    comoving distance :math:`d_C` is given by
+    We can use this to calculate many cosmological distances. For example, the
+    comoving distance :math:`d_{{C}}` is given by
 
     .. math::
 
@@ -223,7 +223,7 @@ def analytic_integral(z, Om0, w0=-1, zpower=0):
 
     >>> analytic_integral(z=2,Om0=0.3,zpower=0) * wcosmo.hubble_distance(H0=70)
 
-    We can check this against the comoving volume given by ``astropy`` and see 
+    We can check this against the comoving volume given by ``astropy`` and see
     that it gives the same answer.
 
     >>> from astropy.cosmology import FlatLambdaCDM
@@ -236,7 +236,7 @@ def analytic_integral(z, Om0, w0=-1, zpower=0):
 
     .. math::
 
-        t_L = t_H\int_0^z \frac{dz'}{(1+z)E(z')}
+        t_{{L}} = t_{{H}}\int_{{0}}^{{z}} \frac{dz'}{(1+z)E(z')}
 
     where :math:`t_L` is the Hubble distance. In this case, :math:`k=-1`
 
@@ -246,16 +246,16 @@ def analytic_integral(z, Om0, w0=-1, zpower=0):
 
     >>> ap_cosmo.lookback_time(2)
 
-    Finally, we can calculate the absorption distance, :math:`X`, commonly used 
-    in absorption line spectroscopy. This is not a physical distance, but is still 
+    Finally, we can calculate the absorption distance, :math:`X`, commonly used
+    in absorption line spectroscopy. This is not a physical distance, but is still
     a very useful quantity.
-    See the book "Cosmological Absorption Line Spectroscopy" by Christopher W. 
+    See the book "Cosmological Absorption Line Spectroscopy" by Christopher W.
     Churchill for a full discussion.
 
     .. math::
 
         X(z, \Omega_m) = \int_0^{z} \frac{dz' (1+z')^2}{
-            \sqrt{\Omega_m(1+z'^3) + (1 − \Omega_m)}}
+            \sqrt{\Omega_m(1+z'^3) + (1 - \Omega_m)}}
 
     Here, :math:`k=2` and there are no factors of the Hubble distance.
 

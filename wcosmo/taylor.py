8e71bacacdaed36872126b43df24cd74b095e10c
Amanda Farah <amanda.farah@ligo.org>
2024-08-12 14:37:03 -0500

    add ouptputs of examples, wcosmo equivalents of astropy commands

--- wcosmo/taylor.py
+++ wcosmo/taylor.py
@@ -227,6 +227,7 @@ def analytic_integral(z, Om0, w0=-1, zpower=0):
     >>> import wcosmo
 
     >>> analytic_integral(z=2,Om0=0.3,zpower=0) * wcosmo.hubble_distance(H0=70)
+    5179.8621
 
     We can check this against the comoving volume given by ``astropy`` and see
     that it gives the same answer.
@@ -234,8 +235,12 @@ def analytic_integral(z, Om0, w0=-1, zpower=0):
     >>> from astropy.cosmology import FlatLambdaCDM
     >>> ap_cosmo = FlatLambdaCDM(H0=70,Om0=0.3)
     >>> ap_cosmo.comoving_distance(2)
+    5179.8621 Mpc
 
-    This is how :function:`comoving_distance` is implemented in ``wcosmo``.
+    This is how :function:`comoving_distance` is implemented in ``wcosmo``, which gives the same result
+
+    >>> wcosmo.comoving_distance(2,H0=70,Om0=0.3)
+    5179.8621
 
     As another example, the lookback time :math:`t_L` is given by
 
@@ -246,10 +251,13 @@ def analytic_integral(z, Om0, w0=-1, zpower=0):
     where :math:`t_L` is the Hubble distance. In this case, :math:`k=-1`
 
     >>> analytic_integral(z=2,Om0=0.3,zpower=-1) * wcosmo.hubble_time(70) # Gyr
-
-    We can again compare this to ``astropy``
+    10.240357
+    We can again compare this to ``astropy`` and ``wcosmo``'s built-in commands
 
     >>> ap_cosmo.lookback_time(2)
+    10.240357 Gyr
+    >>> wcosmo.lookback_time(2,H0=70,Om0=0.3)
+    10.240357
 
     Finally, we can calculate the absorption distance, :math:`X`, commonly used
     in absorption line spectroscopy. This is not a physical distance, but is still
@@ -265,7 +273,11 @@ def analytic_integral(z, Om0, w0=-1, zpower=0):
     Here, :math:`k=2` and there are no factors of the Hubble distance.
 
     >>> print(analytic_integral(z=2,Om0=0.3,zpower=2))
+    4.36995
     >>> print(ap_cosmo.absorption_distance(2))
+    4.36995
+    >>> wcosmo.absorption_distance(2,Om0=0.3)
+    4.36995
     """
     kwargs = dict(Om0=Om0, w0=w0, zpower=zpower)
     return indefinite_integral(z, **kwargs) - indefinite_integral(0, **kwargs)

b423d828b359eaa8aab9db80c8eddce17281e84f
Colm Talbot <talbotcolm@gmail.com>
2024-10-04 11:21:16 -0500

    REVERT: no longer directly subclass astropy to avoid units issues (#21)
    * REVERT: no longer directly subclass astropy to avoid units issues
    
    * TEST: make astropy jit test more sensible

--- wcosmo/test/test_astropy.py
+++ wcosmo/test/test_astropy.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_astropy_cosmology_not_clobbered():
     """See https://github.com/ColmTalbot/wcosmo/issues/15"""
     import astropy.cosmology
@@ -5,3 +8,30 @@ def test_astropy_cosmology_not_clobbered():
     import wcosmo.astropy
 
     assert "wcosmo" not in astropy.cosmology.Planck15.__module__
+
+
+def test_jits():
+    pytest.importorskip("jax")
+    import gwpopulation
+    from astropy.cosmology import FlatLambdaCDM
+    from jax import jit
+
+    import wcosmo
+    from wcosmo.astropy import FlatwCDM
+    from wcosmo.utils import disable_units
+
+    @jit
+    def test_func(h0):
+        cosmo = FlatwCDM(h0, 0.1, -1)
+        return cosmo.luminosity_distance(0.1)
+
+    gwpopulation.set_backend("jax")
+    disable_units()
+
+    assert (
+        abs(
+            float(test_func(67.0))
+            - FlatLambdaCDM(67.0, 0.1).luminosity_distance(0.1).value
+        )
+        < 1
+    )
